68857c2b0c374efd6166ddfd53e5a095e4ac19c9
Carlos Alaniz <YZRN7B@NAM.corp.gm.com>
2018-01-24 13:20:34 -0600

    Service manager into namespace

--- config/ServiceManager.ts
+++ config/ServiceManager.ts
@@ -1,19 +1,21 @@
 import { MessensageRepository } from './../src/bot_hooks/facebook/repositories/MessageRepository';
 import { RedisPersistance } from "../src/common/RedisPersistance";
-import { StateManager } from '../src/common/StateManager';
-
-export class ServiceManager {
-    static PersistanceService: IPersistance =
+import { StateManager as StMgr } from '../src/common/StateManager';
+export namespace ServiceManager {
+    const global: {
+        [key: string]: any
+    } = {}
+    export var PersistanceService: IPersistance =
         (function (): IPersistance {
-            if ((<any>global)["3368bb50-43a3-493e-a012-76aeebee9f80"] == null) {
+            if (global["3368bb50-43a3-493e-a012-76aeebee9f80"] == null) {
                 (<any>global)["3368bb50-43a3-493e-a012-76aeebee9f80"] = new RedisPersistance();
             }
             return <IPersistance>(<any>global)["3368bb50-43a3-493e-a012-76aeebee9f80"];
         })();
-    static MessageRepository: IMessageRepository<IMessageEventDto> = new MessensageRepository();
-    static StateManager: IStateManager = (function (): IStateManager {
+    export var MessageRepository: IMessageRepository<IMessageEventDto> = new MessensageRepository();
+    export var StateManager: IStateManager = (function (): IStateManager {
         if ((<any>global)["fe5b6785-40e6-4ed2-abb2-db1c93950b0a"] == null) {
-            (<any>global)["fe5b6785-40e6-4ed2-abb2-db1c93950b0a"] = new StateManager();
+            (<any>global)["fe5b6785-40e6-4ed2-abb2-db1c93950b0a"] = new StMgr();
         }
         return <IStateManager>(<any>global)["fe5b6785-40e6-4ed2-abb2-db1c93950b0a"];
     })();
